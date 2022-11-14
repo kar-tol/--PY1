e5768bfb5e44644ca1ff00736fe7f510a867e650
kar-tol <115095057+kar-tol@users.noreply.github.com>
2022-11-14 17:44:15 +0300

    Update task_3.py

--- Лабораторная работа 5/task_3.py
+++ Лабораторная работа 5/task_3.py
@@ -1,13 +1,10 @@
 from random import randint  # Из модуля random импортируем функцию randint
 
 
-start = -10  # Начало диапазона значений
-stop = 10  # Конец диапазона значений
-count = 15  # Размер списка
 list_numbers = []  # Список, который будем заполнять значениями
 
 
-def get_unique_list_numbers() -> list[int]:  # Создаем функцию, которая возвращает список, заполненный случайными целыми числами
+def get_unique_list_numbers(start=-10, stop=10, count=15) -> list[int]:  # Создаем функцию, которая возвращает список, заполненный случайными целыми числами
     while len(list_numbers) != count:
         num = randint(start, stop)
         while num not in list_numbers:
